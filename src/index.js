213656722dfde934a94aafea299b05e130610027
Brian Aston <astonb.dev@gmail.com>
2022-02-28 14:59:47 -0800

    Logged fruits

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@
 
 import { fruits } from "./foods";
 import { choice, remove } from "./helpers";
+
+let fruit = choice(fruits);
+remove(fruits, fruit);
+
+console.log(`“I’d like one ${fruit}, please.”`);
+console.log(`“Here you go: ${fruit}”`);
+console.log(`“Delicious! May I have another?”`);
+console.log(`“I’m sorry, we’re all out. We have ${fruits} left.`);
